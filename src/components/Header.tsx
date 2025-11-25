3f88eaedbe32de5e88f7491270711d741a8fcb9c
sosoku321 <phuchuynguyenchau@gmail.com>
2025-11-25 22:14:34 +0700

    Update header style and text ver 2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,11 @@ export default function Header() {
   return (
     <header style={{ padding: "20px", background: "#d1e7ff" }}>
       <h1>Updated Header with new style!</h1>
-      <p>This is version 2 of the header.</p>
+      <p>This is version 2 of the headers.</p>
+      <br />
+      <br />
+      <p>This is version 3 of the header.</p>
+      <p>This is version 4 of the header.</p>
     </header>
   );
 }
